ae31ee795da426df0a454667962dc5d739a82f39
엄태권 <akfls221@gmail.com>
2022-08-09 03:06:29 +0900

    feat : Member Entity Builder 생성

--- src/main/java/com/example/spring_jpa_miniproject/domain/Member.java
+++ src/main/java/com/example/spring_jpa_miniproject/domain/Member.java
@@ -1,9 +1,14 @@
 package com.example.spring_jpa_miniproject.domain;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 
 @Entity
+@NoArgsConstructor
 public class Member {
 
     @Id
@@ -17,4 +22,11 @@ public class Member {
 
     @OneToMany(mappedBy = "member")
     private List<Order> orderList = new ArrayList<>();
+
+    @Builder
+    public Member(String name, Address address, List<Order> orderList) {
+        this.name = name;
+        this.address = address;
+        this.orderList = orderList;
+    }
 }
