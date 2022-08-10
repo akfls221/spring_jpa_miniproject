87b82c54fb41dbc42dc6f87ef762007500f0089e
엄태권 <akfls221@gmail.com>
2022-08-11 02:02:12 +0900

    feat : Order Entity 생성

---
+++ src/main/java/com/example/spring_jpa_miniproject/domain/Orders.java
@@ -0,0 +1,34 @@
+package com.example.spring_jpa_miniproject.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+@NoArgsConstructor
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    private List<OrderItems> orderItemsList = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private LocalDateTime orderDate;
+}
