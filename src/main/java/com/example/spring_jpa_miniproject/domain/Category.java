7291681f96f030b3cdc24a43e07b46c6666b449b
엄태권 <akfls221@gmail.com>
2022-08-12 02:36:30 +0900

    feat : Category Entity 생성

---
+++ src/main/java/com/example/spring_jpa_miniproject/domain/Category.java
@@ -0,0 +1,45 @@
+package com.example.spring_jpa_miniproject.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
+    //계층형 테이블 구조 및 쿼리
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+}
