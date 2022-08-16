92590058d6cd3c550a208aefc2f2ab93f331ea03
엄태권 <akfls221@gmail.com>
2022-08-17 02:48:59 +0900

    feat : Item Entity 재고 관리용 비지니스 로직 추가 및 업데이트 편의 메서드 추가

--- src/main/java/com/example/spring_jpa_miniproject/domain/Item.java
+++ src/main/java/com/example/spring_jpa_miniproject/domain/Item.java
@@ -1,6 +1,9 @@
 package com.example.spring_jpa_miniproject.domain;
 
+import com.example.spring_jpa_miniproject.exception.NotEnoughStockException;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,8 +11,10 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "DTYPE")
+@DiscriminatorColumn
+@NoArgsConstructor
 public abstract class Item {
 
     @Id
@@ -26,4 +31,26 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    /**
+     * 상품 재고 관리용 비지니스 로직 추가
+     */
+    //재고 추가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    //재고 감소
+    public void removeStock(int quantity) throws NotEnoughStockException {
+        if (this.stockQuantity < 0) {
+            throw new NotEnoughStockException("상품의 재고 수량이 없습니다.", 400);
+        }
+        int restStock = this.stockQuantity - quantity;
+        this.stockQuantity = restStock;
+    }
+
+    public void updateItem(Item item) {
+        this.price = item.getPrice();
+        this.stockQuantity = item.getStockQuantity();
+        this.name = item.getName();
+    }
 }
