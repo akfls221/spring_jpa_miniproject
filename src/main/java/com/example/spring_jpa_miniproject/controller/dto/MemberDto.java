3169c9b5f4eed828826bfc14e1e8c43f21ee8a47
엄태권 <akfls221@gmail.com>
2022-08-17 02:50:21 +0900

    fix : MemberDto Embedded 어노테이션 제거

--- src/main/java/com/example/spring_jpa_miniproject/controller/dto/MemberDto.java
+++ src/main/java/com/example/spring_jpa_miniproject/controller/dto/MemberDto.java
@@ -3,14 +3,11 @@ package com.example.spring_jpa_miniproject.controller.dto;
 import com.example.spring_jpa_miniproject.domain.Address;
 import lombok.Getter;
 
-import javax.persistence.Embedded;
-
 @Getter
 public class MemberDto {
 
     private String name;
 
-    @Embedded
     private Address address;
 
     public MemberDto(String name, Address address) {
