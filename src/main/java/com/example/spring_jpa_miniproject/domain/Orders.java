975e71f69d163891c9c8300236a0876197739831
엄태권 <akfls221@gmail.com>
2022-08-19 02:22:06 +0900

    feat : 주문 생성 및 취소 로직 작성

--- src/main/java/com/example/spring_jpa_miniproject/domain/Orders.java
+++ src/main/java/com/example/spring_jpa_miniproject/domain/Orders.java
@@ -1,11 +1,13 @@
 package com.example.spring_jpa_miniproject.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -34,11 +36,54 @@ public class Orders {
 
     private LocalDateTime orderDate;
 
+    /**
+     * 주문 생성 메서드
+     */
+    public static Orders createOrder(Member member, Delivery delivery, OrderItems... orderItems) {
+        Orders order = Orders.builder()
+                .member(member)
+                .delivery(delivery)
+                .orderStatus(OrderStatus.ORDER)
+                .orderDate(LocalDateTime.now())
+                .build();
+        for (OrderItems orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        return order;
+    }
+
+    /**
+     * 주문 취소 메서드
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalArgumentException("이미 배송완료된 상품은 취소가 불가능 합니다.");
+        }
+
+        this.orderStatus = OrderStatus.CANCEL;
+        for (OrderItems orderItem : orderItemsList) {
+            orderItem.cancel();
+        }
+    }
+
+    private void addOrderItem(OrderItems orderItem) {
+        this.getOrderItemsList().add(orderItem);
+
+    }
+
     //연관관계 편의 메서드
     public void setMember(Member member) {
         this.member = member;
         if (!member.getOrderList().contains(this)) {
             member.addOrder(this);
         }
+    }
+
+    @Builder
+    public Orders(Member member, Delivery delivery, OrderStatus orderStatus, LocalDateTime orderDate) {
+        this.member = member;
+        this.delivery = delivery;
+        this.orderStatus = orderStatus;
+        this.orderDate = orderDate;
     }
 }
